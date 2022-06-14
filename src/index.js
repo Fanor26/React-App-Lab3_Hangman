2f33182499cba2cc1bd353aa05f33b9dd148db25
Fanor1917 <fanormortimer@gmail.com>
2022-06-14 06:43:51 +0000

    hoook useKeyBoard

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import {HangManComponent} from './hangman/HangManComponent'
+import { HangManComponent } from "./hangman/HangManComponent";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <HangManComponent />
-
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HangManComponent />);
